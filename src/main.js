08519274542fa599050c763c7be65346c9278f58
Maximilian Schwarzmüller <maximilianschwarzmuller@Max-MBP.fritz.box>
2020-08-13 18:38:09 +0200

    updated to vue 3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App);
+
+app.mount('#app');
